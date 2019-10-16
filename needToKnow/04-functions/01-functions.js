fbca351cf8fd3e27e4dc983365439ca45231a95d
Karol Polakowski <karpol1245@gmail.com>
2019-10-16 11:47:35 +0200

    Solved functiions tasks

--- needToKnow/04-functions/01-functions.js
+++ needToKnow/04-functions/01-functions.js
@@ -1,7 +1,18 @@
 // CREATE A FUNCTION TAKING TWO numbers AS PARAMETERS AND RETURNING THEIR SUM WITH 3 WAYS:
 
 // - USE A FUNCTION DECLARATION
+function sumNumbersDeclaration(a, b) {
+  return a + b;
+}
 
 // - USE A FUNCTION EXPRESSION
+const sumNumbersExpression = function(a, b) {
+  return a + b;
+};
 
 // - USE AN ARROW FUNCTION
+const sumNumbersArrow = (a, b) => a + b;
+
+console.log(sumNumbersDeclaration(2, 2));
+console.log(sumNumbersExpression(2, 2));
+console.log(sumNumbersArrow(2, 2));
--- needToKnow/04-functions/02-callbacks.js
+++ needToKnow/04-functions/02-callbacks.js
@@ -1,9 +1,16 @@
 // CREATE A FUNCTION sayHello TAKING name AS ARGUMENT, AND PRINTING TO THE CONSOLE "Hello " + name + "!"
-
+const sayHello = name => {
+  console.log(`Hello ${name}!`);
+};
 // CREATE SECOND FUNCTION verifyUser, TAKING name AND callback FUNCTION AS PARAMETER
 // IN CASE THE name IS EQUAL TO "Karol" THEN EXECUTE THE CALLBACK WITH name AS ARGUMENT
 // OTHERWISE PRINT TO THE CONSOLE "Unknown user"
-
+const verifyUser = (name, greeting) => {
+  if (name === "Karol") greeting("Karol");
+  else console.log("Unknown user");
+};
 // CALL verifyUser TWO TIMES:
 // - WITH "Karol" AND sayHello AS ARGUMENTS
-// - WITH "Robert" AND sayHello AS ARGUMENTS 
+// - WITH "Robert" AND sayHello AS ARGUMENTS
+verifyUser("Karol", sayHello);
+verifyUser("Robert", sayHello);
