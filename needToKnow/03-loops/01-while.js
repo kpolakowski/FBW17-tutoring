f73883dc9fe9f2d6c3c52590a92578f585145217
Karol Polakowski <karpol1245@gmail.com>
2019-10-16 10:29:58 +0200

    Solved loops tasks

--- needToKnow/03-loops/01-while.js
+++ needToKnow/03-loops/01-while.js
@@ -1,10 +1,26 @@
-let numbers = [2,4,6,8,10]
+let numbers = [2, 4, 6, 8, 10];
 
 // USE ONLY WHILE LOOP
 
 // PRINT ALL ELEMENTS FROM ARRAY numbers TO THE CONSOLE
-
+let i = 0;
+while (i < numbers.length) {
+  console.log(numbers[i]);
+  i++;
+}
 // REVERSE THE ORDER OF ALL ELEMENTS FROM ARRAY numbers
-
-// SUM NATURAL NUMBERS UNTIL THE SUM IS BIGGER OR EQUAL 100, 
+i = numbers.length - 1;
+while (i >= 0) {
+  console.log(numbers[i]);
+  i--;
+}
+// SUM NATURAL NUMBERS UNTIL THE SUM IS BIGGER OR EQUAL 100,
+i = 1;
+let sum = 0;
+while (sum < 100) {
+  sum += i;
+  i++;
+}
+console.log(sum);
 // PRINT TO THE CONSOLE HOW MANY NUMBERS WERE SUMMED
+console.log(i);
--- needToKnow/03-loops/02-for.js
+++ needToKnow/03-loops/02-for.js
@@ -1,9 +1,17 @@
-let numbers = [2,4,6,8,10]
+let numbers = [2, 4, 6, 8, 10];
 
 // USE ONLY FOR LOOP
 
 // PRINT ALL THE ELEMENTS FROM ARRAY
-
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
 // PRINT ALL THE ELEMENTS IN REVERSE ORDER
+for (let i = numbers.length - 1; i >= 0; i--) {
+  console.log(numbers[i]);
+}
 
 // PRINT ELEMENTS WITH ODD INDEX (WITHOUT USING IF STATEMENT)
+for (let i = 1; i < numbers.length; i += 2) {
+  console.log(numbers[i]);
+}
