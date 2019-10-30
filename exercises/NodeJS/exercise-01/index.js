0541d5a69f4a058cc679589cb609462c8f260424
Karol Polakowski <karpol1245@gmail.com>
2019-10-30 15:52:19 +0100

    Added index.js

---
+++ exercises/NodeJS/exercise-01/index.js
@@ -0,0 +1,16 @@
+const books = [
+  { title: "Makbet", author: "William Shakespeare", price: 10 },
+  { title: "Harry Potter 1", author: "J.K. Rowling", price: 15 },
+  { title: "Harry Potter 2", author: "J.K. Rowling", price: 20 },
+  { title: "A storm of swords", author: "George R.R. Martin", price: 18 },
+  { title: "A feast for crows", author: "George R.R. Martin", price: 15 }
+];
+
+const printBooks = (err, books) => {
+  books.forEach(book => console.log(book));
+  return books;
+};
+const printTotalPrice = books => {
+  console.log(books.reduce((sum, book) => sum + book.price, 0));
+  return books;
+};
