ac7ea898cce60dec191a27c6b7f99bca280e58dc
Christophe Troudart <ch.troudart@gmail.com>
2019-11-12 22:33:02 +0100

    Print greeting with hello

--- needToKnow/05-array-methods/04-filter.js
+++ needToKnow/05-array-methods/04-filter.js
@@ -14,3 +14,17 @@ const filteredPeople = classMate.filter(people => {
 });
 console.log(filteredPeople);
 const peopleBefore1980 = [];
+
+// USE FILTER TO GET ALL OBJECTS REPRESENTING PEOPLE THAT HAVE LETTER 's' or 'S' IN THEIR NAMES
+// SAVE THE RESULT TO ARRAY namesWithS AND PRINT IT TO CONSOLE
+
+const peopleNames = classMate.filter(person => {
+  const namesWiths = [];
+  if (
+    classMate.name === 's'.toLowerCase ||
+    classMate.name === 's'.toUpperCase
+  ) {
+    return person;
+  }
+  console.log(peopleNames);
+});
