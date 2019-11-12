2765d1d638e56c9e3fcb68c46e2d7654f7027f93
Christophe Troudart <ch.troudart@gmail.com>
2019-11-12 20:39:17 +0100

    Step 1 done

--- needToKnow/05-array-methods/04-filter.js
+++ needToKnow/05-array-methods/04-filter.js
@@ -1,14 +1,16 @@
-let users = [
-    { name:"Karol", yearOfBirth:1988 },
-    { name:"Aleksander", yearOfBirth:1980 },
-    { name:"Simon", yearOfBirth:1978 },
-    { name:"Anna", yearOfBirth:1950 },
-    { name:"Ion", yearOfBirth:1988 },
-    { name:"Justin", yearOfBirth:1998 }
-]
+let classMate = [
+  { name: 'Karol', yearOfBirth: 1988 },
+  { name: 'Aleksander', yearOfBirth: 1980 },
+  { name: 'Simon', yearOfBirth: 1978 },
+  { name: 'Anna', yearOfBirth: 1950 },
+  { name: 'Ion', yearOfBirth: 1988 },
+  { name: 'Justin', yearOfBirth: 1998 }
+];
 
 // USE FILTER TO GET ALL OBJECTS REPRESENTING PEOPLE THAT WERE BORN IN THE 1980 OR BEFORE
 // SAVE THE RESULT TO ARRAY peopleBefore1980 AND PRINT IT TO CONSOLE
-
-// USE FILTER TO GET ALL OBJECTS REPRESENTING PEOPLE THAT HAVE LETTER 's' or 'S' IN THEIR NAMES
-// SAVE THE RESULT TO ARRAY namesWithS AND PRINT IT TO CONSOLE
+const filteredPeople = classMate.filter(people => {
+  return people.yearOfBirth <= 1980;
+});
+console.log(filteredPeople);
+const peopleBefore1980 = [];
