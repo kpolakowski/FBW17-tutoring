e56c1a3a12a91531f6dbc19a471e73366e47c78a
Christophe Troudart <ch.troudart@gmail.com>
2019-11-13 12:51:08 +0100

    NeedToKnow -->Array-Mehod --> 01-forEach.js --> Step 1 done

--- needToKnow/05-array-methods/01-forEach.js
+++ needToKnow/05-array-methods/01-forEach.js
@@ -1,10 +1,10 @@
-let names = ['Karol', 'Robert', 'Maria', 'Ion', 'Ajil'];
+let names = ["Karol", "Robert", "Maria", "Ion", "Ajil"];
 
 // USE FOREACH METHOD TO PRINT IN THE CONSOLE GREETING FOR EACH NAME IN ARRAY
 // GREETING FORMAT "Hello Karol"
 
-const greetingsToAlles = names.forEach(item => {
-  console.log(`Hello ${names}`);
+const greetingsToAlles = names.forEach(name => {
+  console.log(`Hello ${name}`);
 });
 
 // USE FOR EACH METHOD TO PRINT TO THE CONSOLE NAMES THAT HAVE ODD INDEX
